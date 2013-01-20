f21fbe49104951414b2977a9d01fb31983027f64
Stanislav <qixi@mail.ru>
2013-01-20 23:08:17 +0400

    fix Matrix3Float

--- src/ru/qixi/api/core/geometry/Matrix3Float.java
+++ src/ru/qixi/api/core/geometry/Matrix3Float.java
@@ -11,17 +11,20 @@ import java.lang.Math;
 
 public class Matrix3Float {
 
-	public static final int	SIZE	= 3;
-	public static final int	LENGTH	= SIZE * 3;
+	public static final int		SIZE	= 3;
+	public static final int		LENGTH	= SIZE * 3;
 
 	private static final int	A		= 0;
 	private static final int	B		= 1;
-	private static final int	TX		= 2;
+	private static final int	M02		= 2;
 	private static final int	C		= 3;
 	private static final int	D		= 4;
-	private static final int	TY		= 5;
+	private static final int	M12		= 5;
+	private static final int	TX		= 6;
+	private static final int	TY		= 7;
+	private static final int	M22		= 8;
 
-	protected final float[]	mMatrix;
+	protected final float[]		mMatrix;
 
 
 	public Matrix3Float() {
@@ -59,15 +62,15 @@ public class Matrix3Float {
 	public void identity() {
 		mMatrix[A] = 1;
 		mMatrix[B] = 0;
-		mMatrix[TX] = 0;
+		mMatrix[M02] = 0;
 
 		mMatrix[C] = 0;
 		mMatrix[D] = 1;
-		mMatrix[TY] = 0;
+		mMatrix[M12] = 0;
 
-		mMatrix[6] = 0;
-		mMatrix[7] = 0;
-		mMatrix[8] = 1;
+		mMatrix[TX] = 0;
+		mMatrix[TY] = 0;
+		mMatrix[M22] = 1;
 	}
 
 
@@ -81,19 +84,19 @@ public class Matrix3Float {
 		final float tty = mMatrix[TY];
 
 		mMatrix[A] = ta * pA + tc * pB;
-		mMatrix[B] = tb * pA + td * pB;		
+		mMatrix[B] = tb * pA + td * pB;
+		mMatrix[M02] = 0;
+
 		mMatrix[C] = ta * pC + tc * pD;
 		mMatrix[D] = tb * pC + td * pD;
+		mMatrix[M12] = 0;
+
 		mMatrix[TX] = ta * pTx + tc * pTy + ttx;
 		mMatrix[TY] = tb * pTx + td * pTy + tty;
-
-		/*
-		 * mMatrix[6] = 0; mMatrix[7] = 0; mMatrix[8] = 1;
-		 */
-
+		mMatrix[M22] = 1;
 	}
-	
-	
+
+
 	public void concat(final Matrix3Float pSource) {
 		concat(pSource.mMatrix[A], pSource.mMatrix[B], pSource.mMatrix[C], pSource.mMatrix[D], pSource.mMatrix[TX], pSource.mMatrix[TY]);
 	}
@@ -125,9 +128,10 @@ public class Matrix3Float {
 
 
 	public void rotate(final float pAngle) {
-		final float sin = (float) Math.sin(pAngle);
-		final float cos = (float) Math.cos(pAngle);
-		concat(cos, sin, -sin, cos, 0.0f, 0.0f);
+		float angle = pAngle * (float)(Math.PI / 180.0f);		
+		final float sin = (float) Math.sin(angle);
+		final float cos = (float) Math.cos(angle);
+		concat(cos, -sin, sin, cos, 0.0f, 0.0f);
 	}
 
 
@@ -137,7 +141,7 @@ public class Matrix3Float {
 
 
 	public void translate(final float pDx, final float pDy) {
-		concat(1.0f, 0.0f, 1.0f, 0.0f, pDx, pDy);
+		concat(1.0f, 0.0f, 0.0f, 1.0f, pDx, pDy);
 	}
 
 }
