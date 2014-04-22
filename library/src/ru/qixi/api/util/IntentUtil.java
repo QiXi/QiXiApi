e1fe94c6e047b92cbb061f17ea65f6c9ace2a9ad
Stas <qixi@mail.ru>
2014-04-23 00:03:42 +0400

    доработка IntentUtil

--- library/src/ru/qixi/api/util/IntentUtil.java
+++ library/src/ru/qixi/api/util/IntentUtil.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.webkit.URLUtil;
 
 
@@ -24,14 +25,13 @@ public class IntentUtil {
 	public static void openPlayStore(final Context context) {
 		String appPackageName = context.getPackageName();
 		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName));
-		if (isIntentAvailable(context, intent)) {
+		if (isIntentAvailable(context, intent))
 			context.startActivity(intent);
-		}
 	}
 
 
 	public static boolean isIntentAvailable(final Context context, final Intent intent) {
-		if (intent != null)
+		if (intent == null)
 			return false;
 		PackageManager packageManager = context.getPackageManager();
 		List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
@@ -49,6 +49,19 @@ public class IntentUtil {
 		intent.setType("message/rfc822");
 		intent.putExtra(Intent.EXTRA_EMAIL, to);
 		intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+		intent.putExtra(Intent.EXTRA_TEXT, text);
+		if (isIntentAvailable(context, intent))
+			context.startActivity(intent);
+	}
+
+
+	public static void shareText(final Context context, final String subject, final String text) {
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		if (!TextUtils.isEmpty(subject)) {
+			intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+		}
 		intent.putExtra(Intent.EXTRA_TEXT, text);
 		if (isIntentAvailable(context, intent))
 			context.startActivity(intent);
