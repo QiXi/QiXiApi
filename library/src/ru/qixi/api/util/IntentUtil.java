4a94788708123dc4815300629e6101d3254d75c6
Stas <qixi@mail.ru>
2014-04-29 01:58:45 +0400

    share с возможностью фильтровать приложения

--- library/src/ru/qixi/api/util/IntentUtil.java
+++ library/src/ru/qixi/api/util/IntentUtil.java
@@ -1,5 +1,6 @@
 package ru.qixi.api.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
@@ -7,7 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.text.TextUtils;
+import android.os.Parcelable;
 import android.webkit.URLUtil;
 
 
@@ -47,24 +48,52 @@ public class IntentUtil {
 	public static void sendEmail(final Context context, final String[] to, final String subject, final String text) {
 		Intent intent = new Intent(Intent.ACTION_SEND);
 		intent.setType("message/rfc822");
+		intent.setPackage("com.google.android.gm");
 		intent.putExtra(Intent.EXTRA_EMAIL, to);
 		intent.putExtra(Intent.EXTRA_SUBJECT, subject);
 		intent.putExtra(Intent.EXTRA_TEXT, text);
+
 		if (isIntentAvailable(context, intent))
 			context.startActivity(intent);
 	}
 
 
 	public static void shareText(final Context context, final String subject, final String text) {
+		shareText(context, subject, text, null, "");
+	}
+
+
+	public static void shareText(final Context context, final String subject, final String text, final ArrayList<String> filterPackage, final String title) {
+		Intent shareIntent = createShareIntent(subject, text);
+		if (filterPackage != null) {
+			List<Intent> targetShareIntents = new ArrayList<Intent>();
+			List<ResolveInfo> matches = context.getPackageManager().queryIntentActivities(shareIntent, 0);
+			for (ResolveInfo info : matches) {
+				String packageName = info.activityInfo.packageName;
+				if (!filterPackage.contains(packageName)) {
+					Intent intent = createShareIntent(subject, text);
+					intent.setPackage(packageName);
+					targetShareIntents.add(intent);
+				}
+			}
+			if (!targetShareIntents.isEmpty()) {
+				Intent intent = Intent.createChooser(targetShareIntents.remove(0), title);
+				intent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toArray(new Parcelable[] {}));
+				context.startActivity(intent);
+			}
+			return;
+		} else if (isIntentAvailable(context, shareIntent))
+			context.startActivity(shareIntent);
+	}
+
+
+	private static Intent createShareIntent(final String subject, final String text) {
 		Intent intent = new Intent();
 		intent.setAction(Intent.ACTION_SEND);
 		intent.setType("text/plain");
-		if (!TextUtils.isEmpty(subject)) {
-			intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-		}
+		intent.putExtra(Intent.EXTRA_SUBJECT, subject);
 		intent.putExtra(Intent.EXTRA_TEXT, text);
-		if (isIntentAvailable(context, intent))
-			context.startActivity(intent);
+		return intent;
 	}
 
 }
