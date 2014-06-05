a94ab150f824f30d1c1e2a244dc03c2bef68b869
burchak <burchak@corp.sputnik.ru>
2014-06-05 12:50:23 +0400

    утилита для обнаружения доменных имен на кириллице

---
+++ library/src/ru/qixi/api/util/DomainNameUtil.java
@@ -0,0 +1,25 @@
+﻿package ru.qixi.api.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.webkit.URLUtil;
+
+public class DomainNameUtil {
+
+	private static final String	DOMAIN_NAME_PATTERN	= "^[A-Za-zА-ЯЁа-яё0-9-]+(\\.[A-Za-zА-ЯЁа-яё0-9-]+)*(\\.[A-Za-zА-ЯЁа-яё]{2,})$";
+	private static Pattern		pattern;
+
+	static {
+		pattern = Pattern.compile(DOMAIN_NAME_PATTERN);
+	}
+
+
+	public static boolean validate(final String url) {
+		if (URLUtil.isNetworkUrl(url))
+			return true;
+		Matcher matcher = pattern.matcher(url);
+		return matcher.matches();
+	}
+
+}
